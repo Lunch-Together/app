fdd6d8ecddf6a4e599830735feb6bd5f37482b74
GwonHyeok <me@ghyeok.io>
2018-11-18 23:00:00 +0900

    텍스트 버튼 클래스 이름 변경

--- src/components/TextButton.js
+++ src/components/TextButton.js
@@ -4,13 +4,13 @@
 import React from 'react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
-export default class AppButton extends React.Component {
+export default class TextButton extends React.Component {
   render() {
     return (
       <View {...this.props.style} style={styles.container}>
-        <TouchableOpacity onPress={ this.props.onPress } activeOpacity={0.6}>
+        <TouchableOpacity onPress={this.props.onPress} activeOpacity={0.6}>
           <View>
-            <Text style={{color: '#ABAEB6'}}>{this.props.name}</Text>
+            <Text style={{ color: '#ABAEB6' }}>{this.props.name}</Text>
           </View>
         </TouchableOpacity>
       </View>
