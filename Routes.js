a240e3326b617995293789f8ede2f827b1f8b48f
GwonHyeok <me@ghyeok.io>
2018-11-25 16:57:47 +0900

    로컬 로그인 페이지 추가

--- Routes.js
+++ Routes.js
@@ -7,14 +7,18 @@ import Login from './src/components/Login.js'
 import Register from './src/components/Register'
 import Home from './src/components/Home'
 import Order from './src/components/Order'
+import QRScanPage from "./src/components/pages/QRScanPage";
+import LocalLoginPage from "./src/components/pages/LocalLoginPage";
 
 const Routes = () => (
   <Router>
     <Scene key="root">
       <Scene key="login" component={Login} title="로그인" initial={true}/>
-      <Scene key="register" component={Register} title="회원가입" />
+      <Scene key="register" component={Register} title="회원가입"/>
       <Scene key="home" component={Home} title="런치투게더"/>
       <Scene key="order" component={Order} title="런치투게더"/>
+      <Scene key="qrScan" component={QRScanPage} title="런치투게더"/>
+      <Scene key="localLogin" component={LocalLoginPage} title="런치투게더"/>
     </Scene>
   </Router>
 );
