634ae3bbb1feddf1992369d418781f4e7168cbc9
GwonHyeok <me@ghyeok.io>
2018-11-27 17:45:25 +0900

    QR Scan 화면 문구 변경

--- src/screens/scanner/QRScanner.js
+++ src/screens/scanner/QRScanner.js
@@ -17,10 +17,13 @@ export default class QRScanner extends React.Component {
     const { hasCameraPermission } = this.state;
 
     if (hasCameraPermission === null) {
-      return <Text>Requesting for camera permission</Text>;
+      return <Text>카메라 권한을 요청합니다</Text>;
     }
     if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
+      return <Text>
+        카메라 사용 권한이 없습니다
+        해당 기능은 카메라 권한이 있어야 사용 가능합니다
+      </Text>;
     }
     return (
       <View style={{ flex: 1 }}>
